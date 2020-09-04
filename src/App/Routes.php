0733d3f6ad048973bd0c03b9086638feb11b5ee7
Manuel Parra <manuelparra@live.com.ar>
2020-09-04 11:17:40 +0200

    termine la consulta de secuencia para cliente

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -2,9 +2,15 @@
 
 use Slim\Routing\RouteCollectorProxy;
 
+$checkProxyHeaders = true;
+$trustedProxies = ['10.0.0.1', '10.0.0.2'];
+
+$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+
 $app->group('/api/v1', function(RouteCollectorProxy $group) {
     $group->get('/users', 'App\Controllers\UsersController:getAll');
 
-    $group->get('/sequences', 'App\Controllers\SequencesController:getAll');
+    $group->get('/sequences', 'App\Controllers\SequencesController:getSecuences');
     $group->get('/sequences/lastcode', 'App\Controllers\SequencesController:getLastSequence');
+    $group->get('/sequences/secuence/{sec}', 'App\Controllers\SequencesController:getSecuence');
 });
--- src/Controllers/SequencesController.php
+++ src/Controllers/SequencesController.php
@@ -4,10 +4,11 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Controllers\BaseController;
 use Endroid\QrCode\QrCode;
+use PDOException;
 
 class SequencesController extends BaseController {
 
-    public function getAll($request, $response, $args) {
+    public function getSecuences($request, $response, $args) {
 
         $pdo = $this->container->get('db');
 
@@ -28,24 +29,108 @@ class SequencesController extends BaseController {
     }
 
     public function getLastSequence($request, $response, $args) {
-        $pdo = $this->container->get('db');
+        try {
+            $pdo = $this->container->get('db');
 
-        $sql = "SELECT MAX(sequences.sequence) AS sequence
-                FROM sequences";
+            $sql = "SELECT MAX(sequences.sequence) AS sequence
+                    FROM sequences";
 
-        $query = $pdo->query($sql);
+            $query = $pdo->query($sql);
 
-        $result = $query->fetch();
-        $sequence = $result->sequence;
+            $result = $query->fetch();
+            $sequence = $result->sequence;
 
-        $url = 'https://numeros.papiro.es/turno/sec=' . $sequence;
+            $url = 'https://numeros.papiro.es/turno/' . $sequence;
 
-        // Creare una nueva instancia de la clase
-        $qrCode = new QrCode($url); // Le paso como parametro el texto recibido via ajax
-        $qrCode->setSize('400 px'); // Alteramos el tamaño por defecto
-        $image = $qrCode->writeString(); // Salida en formato de texto
-        $imageData = base64_encode($image); // Codifico la imagen usando base64_encode
-        $payload = (object) ["src" => "data:image/png;base64," . $imageData]; // Salida del código QR
+            $query = null;
+            $pdo = null;
+
+            // Creare una nueva instancia de la clase
+            $qrCode = new QrCode($url); // Le paso como parametro el texto recibido via ajax
+            $qrCode->setSize('400 px'); // Alteramos el tamaño por defecto
+            $image = $qrCode->writeString(); // Salida en formato de texto
+            $imageData = base64_encode($image); // Codifico la imagen usando base64_encode
+            $payload = (object) ["src" => "data:image/png;base64," . $imageData]; // Salida del código QR
+        } catch ( PDOException $e ) {
+            $payload = '{error} : {"text": ' . $e . '}';
+        }
+
+        $response->getBody()->write(json_encode($payload));
+
+        return $response->withHeader('Content-Type', 'application/json')
+                        ->withStatus(200);
+    }
+
+    public function getSecuence($request, $response, $args) {
+        $ipAddress = $request->getAttribute('ip_address');
+
+        $sec = intval($request->getAttribute('sec'));
+
+        $yourturn = "";
+
+        //$payload = (object) ["sec" => $sec, "ip" => $ipAddress, "fecha" => date("Y-m-d")];
+
+        try {
+            $pdo = $this->container->get('db');
+
+            $sql = "SELECT sequences.sequence, sequences.turn, sequences.ip
+                    FROM sequences
+                    WHERE sequences.sequence = " . $sec . ";";
+
+            $query = $pdo->query($sql);
+
+            $result = $query->fetch();
+
+            if (!$result->sequence) {
+                $yourturn = 'err sne'; // secuencia no existe
+            } else {
+                if (!$result->turn) {
+                    $sql = "SELECT sequences.turn
+                            FROM sequences
+                            WHERE sequences.ip = '" . $ipAddress . "' AND sequences.date_scan_qr = DATE(NOW()) AND sequences.id_status = 2;";
+                    $query = $pdo->query($sql);
+                    $result = $query->fetch();
+                    if ($result->turn) {
+                        $yourturn = $result->turn;
+                    } else {
+                        $sql = "UPDATE sequences AS tableAlpha
+                                INNER JOIN (SELECT " . $sec . " AS sequence, (IFNULL(MAX(sequences.turn), 0) + 1) AS maxturn
+                                            FROM sequences
+                                            WHERE sequences.date_scan_qr = DATE(NOW())) AS tableBeta
+                                ON tableAlpha.sequence = tableBeta.sequence
+                                SET tableAlpha.turn = tableBeta.maxturn,
+                                    tableAlpha.date_scan_qr = DATE(NOW()),
+                                    tableAlpha.time_scan_qr = TIME(NOW()),
+                                    tableAlpha.ip = '" . $ipAddress . "',
+                                    tableAlpha.id_status = 2
+                                WHERE tableAlpha.sequence = " . $sec . ";";
+                        $query = $pdo->query($sql);
+
+                        $sql = "INSERT INTO sequences (sequences.date_reg, sequences.time_reg, sequences.id_status)
+                                VALUES (DATE(NOW()), TIME(NOW()), 1);";
+                        $query = $pdo->query($sql);
+
+                        $sql = "SELECT sequences.turn
+                                FROM sequences
+                                WHERE sequences.sequence = " . $sec . ";";
+
+                        $query = $pdo->query($sql);
+                        $result = $query->fetch();
+                        $yourturn = $result->turn;
+                    }
+                } else {
+                    if ($result->ip == $ipAddress) $yourturn = $result->turn; else $yourturn = 'err soi'; // secuecia otra con otra ip
+                }
+            }
+
+            $query = null;
+            $pdo = null;
+
+            $payload = (object) ["turn" => $yourturn]; // Salida del código QR
+        } catch ( PDOException $e ) {
+            $payload = '{error} : {"text": ' . $e . '}';
+        }
+
         $response->getBody()->write(json_encode($payload));
 
         return $response->withHeader('Content-Type', 'application/json')
