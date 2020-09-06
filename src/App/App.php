deed7b30ac4bba065fea4cbad69147e6b6a68490
Manuel Parra <manuelparra@live.com.ar>
2020-09-06 20:39:00 +0200

    Agrege Middleware para Cors

--- src/App/App.php
+++ src/App/App.php
@@ -12,6 +12,16 @@ $app = AppFactory::create();
 
 $container = $app->getContainer();
 
+// ...
+
+$app->add(\App\Middleware\CorsMiddleware::class); // <--- here
+
+// The RoutingMiddleware should be added after our CORS middleware
+// so routing is performed first
+$app->addRoutingMiddleware();
+
+// ...
+
 require __DIR__ . '/Routes.php';
 require __DIR__ . "/Configs.php";
 require __DIR__ . "/Dependencies.php";
--- src/App/Configs.php
+++ src/App/Configs.php
@@ -4,8 +4,8 @@ $container->set('db_settings', function() {
     return (object)[
         "DB_HOST" => "localhost",
         "DB_NAME" => "qr_turn",
-        "DB_USER" => "manuel",
-        "DB_PASS" => "Guti.1712*",
+        "DB_USER" => "qrturnuadm",
+        "DB_PASS" => "7d8l@oD5",
         "DB_CHAR" => "utf8mb4"
     ];
 });
