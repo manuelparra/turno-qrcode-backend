d6b6dd5a2fad50e0e133b3579279a31b81f93401
Manuel Parra <manuelparra@live.com.ar>
2020-09-06 13:07:49 +0200

    Arregle un bug

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -9,7 +9,6 @@ $app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
 
 $app->group('/api/v1', function(RouteCollectorProxy $group) {
     $group->get('/users', 'App\Controllers\UsersController:getAll');
-
     $group->get('/sequences', 'App\Controllers\SequencesController:getSecuences');
     $group->get('/sequences/lastcode', 'App\Controllers\SequencesController:getLastSequence');
     $group->get('/sequences/secuence/{sec}', 'App\Controllers\SequencesController:getSecuence');
